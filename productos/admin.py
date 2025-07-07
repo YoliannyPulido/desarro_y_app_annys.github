52b39f0b4d258f5ea02da4679ecec91606d138c5
YoliannyPulido <yoliannypulido@gmail.com>
2025-07-07 00:11:34 -0400

    Delete productos directory

--- productos/admin.py
+++
@@ -1,8 +0,0 @@
-# productos/admin.py
-
-from django.contrib import admin
-from .models import Producto, Cart, CartItem # <<-- ¡Importa los nuevos modelos!
-
-admin.site.register(Producto)
-admin.site.register(Cart) # <<-- ¡Registra el modelo Cart!
-admin.site.register(CartItem) # <<-- ¡Registra el modelo CartItem!
